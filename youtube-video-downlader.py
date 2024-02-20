3638cfa4664046bb318cddca2fed770b72e5531f
chege a <98897096+djikdjik@users.noreply.github.com>
2024-02-20 15:57:00 +0300

    Create youtube-video-downlader.py

---
+++ youtube-video-downlader.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("View: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+yd.download('C:/Users/dijk/Videos/youtube-py-downloads')
+
+print("Download Sucessful: "\, yt.title ")
+
