90104a5e4646f234817687c4acb7bd60e26a3637
Chege A <achege758@gmail.com>
2024-02-25 18:31:24 +0300

    added file size

--- youtube-mp4-video-downloader.py
+++ youtube-mp4-video-downloader.py
@@ -9,6 +9,8 @@ print("Title: ", yt.title)
 print("View: ", yt.views)
 
 yd = yt.streams.get_highest_resolution()
+file_size = round((yd.filesize / 1000000), 2)
+print(f"File Size: {file_size} MB")
 yd.download('C:/Users/dijk/Videos/youtube-py-downloads')
 
 print("Download Successful: ", yt.title)
